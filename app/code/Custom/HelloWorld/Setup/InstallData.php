f7ad969f584a6793e1ae11669b8143f04d19ad06
addams <addams.x@infinitytechnologies.com.cn>
2018-05-03 18:07:53 +0800

    v

--- app/code/Custom/HelloWorld/Setup/InstallData.php
+++ app/code/Custom/HelloWorld/Setup/InstallData.php
@@ -21,7 +21,7 @@ class InstallData implements InstallDataInterface
 			'name'         => "How to Create SQL Setup Script in Magento 2",
 			'post_content' => "In this article, we will find out how to install and upgrade sql script for module in Magento 2. When you install or upgrade a module, you may need to change the database structure or add some new data for current table. To do this, Magento 2 provide you some classes which you can do all of them.",
 			'url_key'      => '/magento-2-module-development/magento-2-how-to-create-sql-setup-script.html',
-			'tags'         => 'magento 2,mageplaza helloworld',
+			'tags'         => 'magento 2,custom helloworld',
 			'status'       => 1
 		];
 		$post = $this->_postFactory->create();
---
+++ app/code/Custom/HelloWorld/Setup/Uninstall.php
@@ -0,0 +1,19 @@
+<?php
+namespace Custom\HelloWorld\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class Uninstall implements UninstallInterface
+{
+	public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+	{
+		$installer = $setup;
+		$installer->startSetup();
+
+		$installer->getConnection()->dropTable($installer->getTable('custom_helloworld_post'));
+
+		$installer->endSetup();
+	}
+}
