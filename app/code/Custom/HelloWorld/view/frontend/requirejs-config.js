e37f300edf45f8f57398c42dc4568b93e980dd04
addams <addams.x@infinitytechnologies.com.cn>
2018-05-09 18:19:38 +0800

    v

--- app/code/Custom/HelloWorld/view/frontend/requirejs-config.js
+++ app/code/Custom/HelloWorld/view/frontend/requirejs-config.js
@@ -3,5 +3,23 @@ var config = {
     'mycss': 'Custom_HelloWorld/js/mycss',
     'mycss2': 'Custom_HelloWorld/js/mycss2',
     'mycss3': 'Custom_HelloWorld/js/mycss3',
+    'slick': 'Custom_HelloWorld/js/slick.min'
+  },
+  shim: {
+    'slick': {
+      deps: ['jquery'],
+      exports: 'jQuery.fn.slick', //The value must be a variable defined within the library.
+    }
+  },
+  // When load 'requirejs' always load the following files also
+  deps: [
+    'Custom_HelloWorld/js/main'
+  ],
+  config: {
+    mixins: {
+      'Custom_HelloWorld/js/mycss': {
+        'Custom_HelloWorld/js/mycss-mixins': true
+      }
+    }
   }
 };
---
+++ app/code/Custom/HelloWorld/view/frontend/web/js/main.js
@@ -0,0 +1,1 @@
+console.log('loaded in all page');
