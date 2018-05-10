053b695a6c89ca17590855f7d98ead14ab320f54
addams <addams.x@infinitytechnologies.com.cn>
2018-05-10 08:57:02 +0800

    v

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  addamx: {
+    area: 'frontend',
+    name: 'Addamx/default',
+    locale: 'en_US',
+    files: [
+      'css/styles-m',
+      'css/styles-l',
+      'css/email',
+      'css/email-inline',
+      'css/testLess'
+    ],
+    dsl: 'less'
+  },
+  onlyone: {
+    area: 'frontend',
+    name: 'Addamx/default',
+    locale: 'en_US',
+    files: [
+      'css/style-new'
+    ],
+    dsl: 'less'
+  }
+}
