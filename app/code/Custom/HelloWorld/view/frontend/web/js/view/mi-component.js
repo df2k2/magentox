fb905f0b4486bb319523e1ce2e49a3677fd3d7ba
addams <addams.x@infinitytechnologies.com.cn>
2018-05-15 18:58:18 +0800

    v

--- app/code/Custom/HelloWorld/view/frontend/web/js/view/mi-component.js
+++ app/code/Custom/HelloWorld/view/frontend/web/js/view/mi-component.js
@@ -1,5 +1,5 @@
 define([
-  'uiComponent',
+  'uiComponent',  //module-ui\view\base\web\js\lib\core\collection.js
   'jquery',
   'Magento_Customer/js/customer-data',
   'underscore'
@@ -8,13 +8,26 @@ define([
   'use strict';
 
   return Component.extend({
+      data: {},
       initialize: function () {
           this._super();
+
           if (_.isEmpty(customerData.get('mi-component-data')())) {
-            customerData.set('mi-component-data', {title: 'This mi-component\'s Title'});
+            customerData.set('mi-component-data', {
+              title: 'This mi-component\'s Title',
+              list: [
+                {id: 1, text: 'this is id 1'},
+                {id: 2, text: 'this is id 2'},
+                {id: 3, text: 'this is id 3'},
+              ]
+            });
           }
-          
-          this.miComponent = customerData.get('mi-component-data')();
+          this.data = customerData.get('mi-component-data')();
+
+          return this;
+      },
+      testLog: function() {
+        console.log('testLog')
       }
   });
 });
